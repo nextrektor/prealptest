71d04e23fdd9c0f0edc3ba0d9fef1ac7e7dac88a
natanael <211814901+gabrielnath@users.noreply.github.com>
2025-07-26 21:44:49 +0700

    Uodating Middleware for Webhook

---
+++ app/Http/Middleware/SkipCsrfForWebhook.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class SkipCsrfForWebhook extends Middleware
+{
+    protected $except = [
+        '/greq-web', // exclude GitHub webhook route
+    ];
+}
