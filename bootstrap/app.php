1bfbce274205655700025a0a53a8d05727bd14c2
natanael <211814901+gabrielnath@users.noreply.github.com>
2025-07-26 22:28:54 +0700

    Debugging Auto deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(SkipCsrfForWebhook::class);
+        $middleware->append(App\Http\Middleware\DisableCsrfForWebhook::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
